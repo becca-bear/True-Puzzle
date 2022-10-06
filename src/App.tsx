c620d553e91d0855097862e921f8efab7eb14c04
becca-bear <you@example.com>
2022-10-06 18:11:47 -0400

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./puzzle.css";
+
+const rows: number = 5;
+const columns: number = 5;
+// js array shuffle
 
 function App() {
+  const tiles: React.ReactNode[] = [];
+  for (let r: number = 0; r < 25; r++) {
+    tiles.push(
+      <img
+        src={"./images/blank.jpg"}
+        alt=""
+        key={r}
+        onDragStart={(event) => {
+          console.log("dragstart", event);
+          //event.src
+          //event.preventDefault()
+        }}
+      ></img>
+    );
+  }
+  const pieces: React.ReactNode[] = [];
+  for (let imageNumber = 1; imageNumber < 25; imageNumber++) {
+    pieces.push(
+      <img
+        src={`./images/image_part_${imageNumber
+          .toString()
+          .padStart(3, "0")}.jpg`}
+        alt=""
+        key={imageNumber}
+      ></img>
+    );
+  }
+  const shuffleArray = (pieces: any) => {
+    for (let i = pieces.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = pieces[i];
+      pieces[i] = pieces[j];
+      pieces[j] = temp;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="board">{tiles}</div>
+      <h2>
+        Turns: <span id="turns">0</span>
+      </h2>
+      <div id="pieces">{pieces}</div>
     </div>
   );
 }
